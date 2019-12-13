cc6d17133e5c548ac7d47d1af173d1659e6013a3
winslowdibona <dibona.winslow@gmail.com>
2019-12-13 12:09:50 -0500

    Updates

--- watcher/views.py
+++ watcher/views.py
@@ -16,14 +16,5 @@ class PushViewSet(viewsets.ModelViewSet):
 
 
 def index(request):
-    return render(request, "index.html")
-
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
+    pushes = Push.objects.all()
+    return render(request, "index.html", {"pushes": pushes})
