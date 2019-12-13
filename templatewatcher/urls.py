314475b0c1b71aa0e1b851d25cca4c479fb3b190
winslowdibona <dibona.winslow@gmail.com>
2019-12-13 10:17:54 -0500

    Updates

--- templatewatcher/urls.py
+++ templatewatcher/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path, include, url
+from django.urls import path, include
 
 from django.contrib import admin
 
 from rest_framework.routers import DefaultRouter
+
+from django.conf.urls import (
+    url
+)
 
 admin.autodiscover()
 
